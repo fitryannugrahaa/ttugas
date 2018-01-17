2e74772f7d25fb72e54560cf4d2f12ce8ea6e74d
fitryannugrahaa <32404079+fitryannugrahaa@users.noreply.github.com>
2018-01-17 08:33:51 +0800

    hasilnya

--- serigala.cpp
+++ serigala.cpp
@@ -19,3 +19,16 @@ int main(){
 	serigalamerah.jenisMakanan = "Rusa.";
 	serigalarusia.jenisMakanan = "Kijang.";
 	serigalaarktik.jenisMakanan = "Babi Hutan.";
+	
+	cout <<"Serigala Merah berwarna " <<(serigalamerah.warnaBulu);
+	cout <<"makanannya " <<(serigalamerah.jenisMakanan);
+	serigalamerah.bersuara("aauuu");
+	
+	cout <<"Serigala Rusia berwarna " <<(serigalarusia.warnaBulu);
+	cout <<"makanannya " <<(serigalarusia.jenisMakanan);
+	serigalarusia.bersuara("ngauuu");
+}
+
+void serigala::bersuara(string bunyi){
+	cout << " Bunyinya "<<bunyi<< " !!"<<endl;
+}
